6ca25208f5f61a465aed05503c0616a97150149b
Hao-OCT <patrick.wang1025@gmail.com>
2019-05-07 12:25:47 +1000

    register user

--- src/Multiplayer/Server.java
+++ src/Multiplayer/Server.java
@@ -1,5 +1,55 @@
 package Multiplayer;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
+	public static void main(String[] args) {
+		ServerSocket serverSocket;
+		Socket socket = null;
+		DataInputStream in;
+		DataOutputStream out;
+		int count=0;
+		try {
+			serverSocket = new ServerSocket(61099);	
+			System.out.println("The Server is running...");
+			while (true) {
+				socket = serverSocket.accept();
+				count++;
+				System.out.println("The current number of clients is "+count);
+				in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+				out = new DataOutputStream(socket.getOutputStream());
+				out.writeUTF("Please enter your first name to register");
+				//new ServerThread(in.readUTF()).run();
+				out.writeUTF("Please wait for the next round...");
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void register() {
+
+	}
+}
+
+class ServerThread extends Thread {
+	private String text;
+
+	public ServerThread(String text) {
+		this.text = text;
+		setDaemon(true);
+	}
+
+	public void run() {
+		System.out.println("Thread "+text+" is working");
+		this.setName(text);
+		
+	}
 }
