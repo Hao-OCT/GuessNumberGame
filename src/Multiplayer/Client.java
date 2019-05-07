a5a07a5f9ea7e6c4b1b9e4d919b6b74ea3cc0393
Hao-OCT <patrick.wang1025@gmail.com>
2019-05-07 13:34:06 +1000

    Game starts prompts

--- src/Multiplayer/Client.java
+++ src/Multiplayer/Client.java
@@ -10,30 +10,34 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Client {
-public static void main(String[] args) throws IOException {
-	Socket socket = null;
-	BufferedReader reader;
-	DataOutputStream out = null;
-	DataInputStream in = null;
-	try {
-		socket = new Socket("localhost", 61099);
-		reader = new BufferedReader(new InputStreamReader(System.in));
-		out = new DataOutputStream(socket.getOutputStream());
-		in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
-		System.out.println(in.readUTF());
-		out.writeUTF(reader.readLine());
-		System.out.println(in.readUTF());
-
-	} catch (UnknownHostException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} finally {
-		in.close();
-		out.close();
-		socket.close();
+	public static void main(String[] args) throws IOException {
+		Socket socket = null;
+		BufferedReader reader;
+		DataOutputStream out = null;
+		DataInputStream in = null;
+		boolean start = false;
+		try {
+			socket = new Socket("localhost", 61099);
+			reader = new BufferedReader(new InputStreamReader(System.in));
+			out = new DataOutputStream(socket.getOutputStream());
+			in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+			System.out.println(in.readUTF());
+			out.writeUTF(reader.readLine());
+			System.out.println(in.readUTF());
+			while (!start) {
+				start=in.readBoolean();
+			}System.out.println(in.readUTF());
+			System.out.println(in.readUTF());
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			in.close();
+			out.close();
+			socket.close();
+		}
 	}
-}
 }
--- src/Multiplayer/Server.java
+++ src/Multiplayer/Server.java
@@ -6,50 +6,74 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class Server {
+	protected static int count = 0;
+	protected static List<Thread> clients;
 
 	public static void main(String[] args) {
 		ServerSocket serverSocket;
 		Socket socket = null;
 		DataInputStream in;
 		DataOutputStream out;
-		int count=0;
+		clients=new ArrayList<Thread>();
 		try {
-			serverSocket = new ServerSocket(61099);	
+			serverSocket = new ServerSocket(61099);
 			System.out.println("The Server is running...");
 			while (true) {
 				socket = serverSocket.accept();
-				count++;
-				System.out.println("The current number of clients is "+count);
 				in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
 				out = new DataOutputStream(socket.getOutputStream());
 				out.writeUTF("Please enter your first name to register");
-				//new ServerThread(in.readUTF()).run();
-				out.writeUTF("Please wait for the next round...");
+				new ServerThread(socket, in.readUTF()).start();
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-
-	public void register() {
-
-	}
 }
 
 class ServerThread extends Thread {
+	private Socket socket;
 	private String text;
 
-	public ServerThread(String text) {
+	private DataInputStream in;
+	private DataOutputStream out;
+
+	public ServerThread(Socket socket, String text) {
+		this.socket = socket;
 		this.text = text;
 		setDaemon(true);
 	}
 
 	public void run() {
-		System.out.println("Thread "+text+" is working");
-		this.setName(text);
-		
+		try {
+			Server.count++;
+			Server.clients.add(this);
+			System.out.println("The current number of clients is " + Server.count);
+			in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+			out = new DataOutputStream(socket.getOutputStream());
+			
+			out.writeUTF("Please wait for the next round...");
+			System.out.println("Thread " + text + " is working");
+			this.setName(text);
+			while (Server.count < 3) {
+				out.writeBoolean(false);
+			}
+			out.writeBoolean(true);
+			String names = "";
+			for(int i=0;i<Server.clients.size();i++) {
+				names+=Server.clients.get(i).getName()+" ";
+			}
+			out.writeUTF("Game starts and the player lists: "+names);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
